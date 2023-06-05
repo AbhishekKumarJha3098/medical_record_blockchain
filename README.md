c2c0779fc85ea101ed2dcf580593502a382b64eb
Abhishek Kumar Jha <87441996+AbhishekKumarJha3098@users.noreply.github.com>
2023-06-05 14:17:34 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# medical_record_blockchain
+<h1 align="center">Hospital-Records-Blockchain</h1>
+
+Build at: [**HACKFORTOMORROW HACKATHON**](https://hackfortomorrow.devfolio.co/)
+
+
+## Table of Contents:
+- [Introduction](#Introduction)
+- [To Run on Localhost](#LocalImplementation)
+- [Technology Stack](#Technology-Stack)
+- [Features](#Features)
+- [Screenshots and Video](#ScreenshotsandVideo)
+- [Contributors](#Contributors)
+
+## Introduction
+Managing Patient documents across different Hospital is quite hassling. This Dapp, Hospital Blockchain Records helps in maintaining Patient's Medical history. 
+Only Hospital authority has the priviledge to upload  Medical documents for the patients registered in our system. For storing the files, we have used IPFS, which
+is a peer-to-peer distributed system.
+
+## Local Implementation
+1. Clone the Repo and istall dependencies
+```
+https://github.com/somesh-banerjee/Hospital-Records-Blockchain.git
+cd Hospital-Records-Blockchain/frontend/
+npm install
+```
+2. Create a [Infura](https://infura.io/) account and create a new project. Now copy the Project ID from settings Tab and paste it in the marked location in `/ethereum/web3.js` and `/ethereum/deploy.js` files.
+3. Install metamask in your browser and log into your metasmask account.
+4. Run the local webserver with any port.
+```
+npm start
+```
+
+
+## Technology Stack
+1) Ethereum Blockchain
+2) Solidity
+3) Web3 JS
+4) IPFS
+5) Metamask Wallet
+6) React JS
+
+## Features
+1) Decentralized Database for all reports of patients which can be easily accessible by all the hospitals.
+2) As we are storing the Records in blockchain and IPFS it will be untampered by any entity.
+3) No need to provide any proof of authenticity, as the records are in blockchain.
+
+
+## Screenshots and Video
+Home page
+![](./img/Homepage.png)
+Register page
+![](./img/Registerpg.png)
+Hospital page
+![](./img/Hospitalpg.png)
+Patient Page
+![](./img/Patientpage.png)
+Uploading Document
+![](./img/upDoc.png)
+
+
+## Contributors:
+
+Team Name: cryptocoders
+
+* [Somesh Banerjee](https://github.com/somesh-banerjee)
+* [Sayatan Roy](https://github.com/Roy6250)
+* [Vivek Kumar](https://github.com/freakfan15)
+* [Rishi Sounak]()
---
+++ contracts/medic.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: AFL-3.0
+pragma solidity ^0.8.0;
+
+contract superadmin{
+    address public admin;
+    mapping(address => bool) public hosAdmin;
+
+    constructor(){
+        admin = msg.sender;
+    }
+    
+    struct Patient{
+        string name;
+        string aadhar;
+        string dob;
+        string sex;
+        string[] docs;
+        string[] docsD;
+    }
+    mapping(address => Patient) public patients;
+    mapping(address => bool) public patientexist;
+    address[] patList;
+    string[] patNList;
+    
+    struct Hospital{
+        string name;
+    }
+    mapping(address => Hospital) public hospitals;
+    
+    modifier restricted() {
+        require(msg.sender == admin);
+        _;
+    }
+    
+    function newHospital(string memory n,address u) public restricted{
+        require(hosAdmin[u]==false);
+        hospitals[u].name = n;
+        hosAdmin[u]=true;
+    }
+    
+    function newPatient(string memory n,string memory a, string memory d, string memory s) public{
+        require(hosAdmin[msg.sender]==false);
+        require(patientexist[msg.sender]==false);
+        patients[msg.sender].name=n;
+        patients[msg.sender].aadhar=a;
+        patients[msg.sender].dob = d;
+        patients[msg.sender].sex = s;
+        patList.push(msg.sender);
+        patientexist[msg.sender]=true;
+        patNList.push(n);
+    }
+    
+    function upDoc(address pAdd, string memory hash, string memory docD) public{
+        require(hosAdmin[msg.sender]);
+        patients[pAdd].docs.push(hash);
+        patients[pAdd].docsD.push(docD);
+    }
+    
+    function getPatients() public view returns (address[] memory, string[] memory) {
+        return (patList,patNList);
+    }
+
+    function checkAdmin(address add) public view returns (bool) {
+        return hosAdmin[add];
+    }
+    function checkPatient(address add) public view returns (bool) {
+        return patientexist[add];
+    }
+
+    function getPatientDetails(address add) public view returns (string memory,string memory, string memory, string memory, string[] memory, string[] memory) {
+        return (patients[add].name,
+            patients[add].aadhar,
+            patients[add].dob,
+            patients[add].sex,
+            patients[add].docs,
+            patients[add].docsD);
+    }
+
+    function getHosDetails(address add) public view returns (string memory) {
+        return hospitals[add].name;
+    }
+}
+
